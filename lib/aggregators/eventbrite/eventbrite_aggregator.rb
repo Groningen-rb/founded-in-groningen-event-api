49b4fb56c5ae1c131c7184b49db242e4a95ef536
W-M <w-m@gmx.us>
2015-09-22 21:56:43 +0200

    more HTTParty options.

--- lib/aggregators/eventbrite/eventbrite_aggregator.rb
+++ lib/aggregators/eventbrite/eventbrite_aggregator.rb
@@ -1,8 +1,17 @@
 class EventbriteAggregator
 	include HTTParty
-	base_uri "https://www.eventbriteapi.com/v3/events/search/"
+	base_uri "https://www.eventbriteapi.com/v3/"
 
   def initialize(service, page)
-    @options = { token: Rails.application.secrets.eventbrite_token }
+    @options = { 
+    	:token 						=> Rails.application.secrets.eventbrite_token, 
+    	:"location.latitude"		=> 53.2167, 
+    	:"location.longitude"		=> 6.5667, 
+    	:"start_date.range_start"	=> DateTime.now.to_s
+    }
+  end
+
+  def fetch
+  	self.class.get("events/search/", @options)
   end
 end
