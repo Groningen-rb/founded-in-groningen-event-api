1ea3ee39b16901f4369c67e9fb7f0709b4f6a148
W-M <w-m@gmx.us>
2015-09-22 22:44:26 +0200

    Working JSON-parsing.

--- lib/aggregators/eventbrite/eventbrite_aggregator.rb
+++ lib/aggregators/eventbrite/eventbrite_aggregator.rb
@@ -1,36 +1,18 @@
 require 'rest-client'
+require "json"
 
 class EventbriteAggregator
-=begin
-	include HTTParty
-	base_uri "https://www.eventbriteapi.com/v3/"
 
-  def initialize
-    @options = { 
-    	#:token 						=> Rails.application.secrets.eventbrite_token, 
-    	:"location.latitude"		=> 53.2167, 
-    	:"location.longitude"		=> 6.5667, 
-    	:"start_date.range_start"	=> DateTime.now.to_s,
-    	headers:{
-    		#{}"Accept" =>  "application/json"
-    		"Authorization"				=>  "Bearer #{Rails.application.secrets.eventbrite_token}"
-    	}
-    }
-    puts @options
-  end
-
-  def fetch
-  	raw_result = self.class.get("events/search/", @options)
-  	result = JSON.parse(raw_result)
-  end
-=end
 	def fetch
 		params = {
 	    	"location.latitude"		=> 53.2167, 
 	    	"location.longitude"		=> 6.5667, 
 	    }
 
-		RestClient.get "https://www.eventbriteapi.com/v3/events/search/", {params: params, accept: :json,  authorization: "Bearer #{Rails.application.secrets.eventbrite_token}"}
+		raw_response =RestClient.get "https://www.eventbriteapi.com/v3/events/search/", {params: params, accept: :json,  authorization: "Bearer #{Rails.application.secrets.eventbrite_token}"}
+		response = JSON.parse(raw_response);
+
+		
 	end
 
 end
