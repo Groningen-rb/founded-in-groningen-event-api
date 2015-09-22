4b749477938eef65e31388addaa0c331470699c3
Ivan Malykh <ivan@lesslines.com>
2015-09-22 22:09:34 +0200

    added expected implementation and expectations

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,9 @@
 class EventsController < ApplicationController
+  def index
+    futures = Aggregators.all_aggregators.map do |aggregator|
+      aggregator.new.future.fetch
+    end
+
+    @events = futures.map(&:value).flatten
+  end
 end
