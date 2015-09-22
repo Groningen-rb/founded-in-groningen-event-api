0bdb39d3dcfc0f84d66aff9fd3e7fb2f894f8aa2
W-M <w-m@gmx.us>
2015-09-22 21:47:08 +0200

    Start of HTTPParty settings.

--- lib/aggregators/eventbrite/eventbrite_aggregator.rb
+++ lib/aggregators/eventbrite/eventbrite_aggregator.rb
@@ -1,4 +1,8 @@
 class EventbriteAggregator
 	include HTTParty
-	base_uri ""
+	base_uri "https://www.eventbriteapi.com/v3/events/search/"
+
+  def initialize(service, page)
+    @options = { token: Rails.application.secrets.eventbrite_token }
+  end
 end
